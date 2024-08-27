08f01e0251c1dec4e0696f41a248df1c1cca87b8
Saul Alejos Garay <saulalejos1984@gmail.com>
2024-08-27 12:44:30 -0300

    evento mostrar

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,22 @@
-console.log("nodemon  ")
+//instance ICON
+feather.replace()
+
+
+const mostrarCarrito=document.querySelector(".nav-item-card").addEventListener("click",showHiddenList);
+
+
+const mostrarLista=document.querySelector(".nav-item-card-list");
+    
+function showHiddenList(e){
+    e.preventDefault();
+    e.stopPropagation();
+    console.log("hola")
+         if(mostrarLista.classList.contains("nav-item-card-list")){
+            mostrarLista.classList.add("show-card");
+            //mostrarLista.classList.remove("hidden-card");
+            
+        }else{
+            //mostrarLista.classList.remove("show-card");
+            mostrarLista.classList.add("hidden-card");
+}
+}
