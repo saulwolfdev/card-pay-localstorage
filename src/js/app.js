c00fdde6dacd9106eee44cd635fb073069b904e2
Saul Alejos Garay <saulalejos1984@gmail.com>
2024-08-28 18:28:29 -0300

    seleccion de elemento y creacion de objeto

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,60 @@
 //instance ICON
 feather.replace()
 
-
-const mostrarCarrito=document.querySelector(".nav-item-card").addEventListener("click",showHiddenList);
-
-
+//contenedor Menu
+const mostrarCarrito=document.querySelector(".nav-item-card")
+      mostrarCarrito.addEventListener("click",showHiddenList);
 const mostrarLista=document.querySelector(".nav-item-card-list");
-    
+const limpiarCarrito=document.querySelector(".nav-item-card-list-clear");
+
+
+
+
+const agregarElementoCarrito=document.querySelector(".card-item");
+
+//TODOS LOS EVENTOS
+cargarRegistrarEventos();
+
+function cargarRegistrarEventos(){
+    agregarElementoCarrito.addEventListener("click",agregarCarritoProducto);
+   
+} 
+
+function agregarCarritoProducto(e){
+    e.preventDefault()
+    if(e.target.classList.contains("card-item-button")){
+        const elementoSeleccionado=e.target.parentElement;
+        leerDatosSeleccionados(elementoSeleccionado);
+    }
+}
+
+function leerDatosSeleccionados(elementoSeleccionado){
+   //console.log("CLICK U TOMA DE HTML",elementoSeleccionado)
+
+   const creacionDeELemento={
+        imagen:elementoSeleccionado.querySelector("img").src,
+        titulo:elementoSeleccionado.querySelector("h4").textContent,
+        parrafo:elementoSeleccionado.querySelector("p").textContent,
+        precio:elementoSeleccionado.querySelector("button").getAttribute("data-id"),
+        cantidad:1
+   }
+   console.log("Creacion de Objeto Seleccionado",creacionDeELemento)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function showHiddenList(e){
     e.preventDefault();
     e.stopPropagation();
